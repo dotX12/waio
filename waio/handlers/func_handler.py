56b6915acc7d93756591c245260dee046ae1fe32
dotX12 <hash@shitposting.team>
2021-11-01 11:11:16 +0300

    FromFuncHandler

---
+++ waio/handlers/func_handler.py
@@ -0,0 +1,28 @@
+from typing import Callable, Union, Any
+from waio.rules.abc import ABCMessageRule
+
+
+class FromFuncHandler:
+    def __init__(self, handler: Callable, *rules: ABCMessageRule):
+        self.handler = handler
+        self.rules = rules
+
+    async def filter(self, event: Any) -> Union[dict, bool]:
+        rule_context = {}
+        for rule in self.rules:
+            result = await rule.check(event)
+            if result is False or result is None:
+                return False
+            elif result is True:
+                continue
+            rule_context.update(result)
+        return rule_context
+
+    async def handle(self, **context) -> Any:
+        return await self.handler(**context)
+
+    def __repr__(self):
+        return (
+            f"<FromFuncHandler {self.handler.__name__} "
+            f"rules={self.rules}>"
+        )
