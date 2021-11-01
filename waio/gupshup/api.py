2df69a30b0e104c7515352baab402572a89b3a61
dotX12 <hash@shitposting.team>
2021-11-01 11:13:00 +0300

    gupshup settings base class

---
+++ waio/gupshup/api.py
@@ -0,0 +1,14 @@
+class GupshupSettings:
+    def __init__(self, apikey: str, src_name: str, phone_number: int):
+        self.apikey = apikey
+        self.src_name = src_name
+        self.phone_number = phone_number
+
+    def _headers(self):
+        headers = {
+            'Cache-Control': 'no-cache',
+            'cache-control': 'no-cache',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'apikey': self.apikey
+        }
+        return headers
