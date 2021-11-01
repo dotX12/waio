3d878d0753330855d9bdedf0b86d589b32ac762a
dotX12 <hash@shitposting.team>
2021-11-01 11:16:17 +0300

    data form generator

---
+++ waio/gupshup/form.py
@@ -0,0 +1,14 @@
+import json
+
+from waio.utils.form_data import CustomFormData
+
+
+def generate_message_form(source, receiver, app_name, message: json) -> CustomFormData:
+    form = CustomFormData()
+    form.add_www_form(name='channel', value='whatsapp')
+    form.add_www_form(name='source', value=source)
+    form.add_www_form(name='destination', value=receiver)
+    form.add_www_form(name='src.name', value=app_name)
+    form.add_www_form(name='message', value=message)
+
+    return form
