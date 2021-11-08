e7b66576e274034b3f3d751acbad6e825441ea9c
dotX12 <hash@shitposting.team>
2021-11-09 02:09:30 +0300

    del __init__ in ABCRule

--- waio/rules/abc.py
+++ waio/rules/abc.py
@@ -5,9 +5,5 @@ from typing import Union
 class ABCRule(ABC):
 
     @abstractmethod
-    def __init__(self, *args, **kwargs):
-        pass
-
-    @abstractmethod
     async def check(self, message) -> Union[dict, bool]:
         pass
