b70c0b30b289ce0fb03cc8f89012cb9a7ef77e9e
dotX12 <hash@shitposting.team>
2021-11-09 18:59:19 +0300

    added method send_list to protocol

--- waio/protocols/bot.py
+++ waio/protocols/bot.py
@@ -1,7 +1,11 @@
 from typing import Protocol
+from waio.keyboard.list import ListMessage
 
 
 class Bot(Protocol):
 
     async def send_message(self, receiver: int, message: str):
         """Send message"""
+
+    async def send_list(self, receiver: int, button: ListMessage):
+        """Send list message"""
