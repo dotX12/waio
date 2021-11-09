e25a8f4eb3969fa1834a2328cd32f85ef4e8a2ad
dotX12 <hash@shitposting.team>
2021-11-09 21:23:50 +0300

    added dict, json method to MessageText

--- waio/models/text.py
+++ waio/models/text.py
@@ -1,8 +1,13 @@
+import ujson
+
 
 class MessageText:
     def __init__(self, text: str):
         self.type = 'text'
         self.text = text
 
-    def json(self):
+    def dict(self):
         return {"type": self.type, "text": self.text}
+
+    def json(self):
+        return ujson.dumps(self.dict(), indent=2)
