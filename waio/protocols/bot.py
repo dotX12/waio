d008d433eb86b2d6504c86dafc3cd69aded75c14
dotX12 <hash@shitposting.team>
2021-11-09 22:22:18 +0300

    added method send_reply

--- waio/protocols/bot.py
+++ waio/protocols/bot.py
@@ -1,5 +1,6 @@
 from typing import Protocol
 from waio.keyboard.list import ListMessage
+from waio.keyboard.reply import QuickReply
 
 
 class Bot(Protocol):
@@ -7,5 +8,8 @@ class Bot(Protocol):
     async def send_message(self, receiver: int, message: str):
         """Send message"""
 
-    async def send_list(self, receiver: int, button: ListMessage):
+    async def send_list(self, receiver: int, keyboard: ListMessage):
         """Send list message"""
+
+    async def send_reply(self, receiver: int, keyboard: QuickReply):
+        """Send reply message"""
