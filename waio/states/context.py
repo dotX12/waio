46d72df5250d4fc89ab0a56d3d71b28988145e18
dotX12 <hash@shitposting.team>
2021-11-11 10:12:28 +0300

    Added get_data; clear_data; clear_data - argument in finish for clear data

--- waio/states/context.py
+++ waio/states/context.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, Optional, Dict, Any
 
 from waio.states.fsm import BaseState
 from waio.storage.redis import RedisStorage
@@ -15,12 +15,16 @@ class FSMContext:
     async def get_state(self) -> str:
         return await self.storage.get_state(key=self.user)
 
-    async def finish(self):
-        return await self.storage.finish_state(key=self.user)
+    async def finish(self, clear_data: Optional[bool] = False) -> None:
+        await self.storage.finish_state(key=self.user)
+        if clear_data:
+            await self.storage.finish_data(key=self.user)
 
-    async def set_data(self, **kwargs: Union[str, int]):
+    async def set_data(self, **kwargs: Union[str, int]) -> bool:
         return await self.storage.set_data(self.user, **kwargs)
 
-    async def get_data(self):
-        key = self.storage.generate_key_data(key=self.user)
-        return await self.storage.base_get(key)
+    async def get_data(self, *data_keys) -> Dict[str, Union[str, int]]:
+        data_values = await self.storage.get_data(key=self.user)
+        if data_keys:
+            return {key: value for key, value in data_values.items() if key in data_keys}
+        return data_values
