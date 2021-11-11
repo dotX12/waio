a58fe37b0bc1868ddda60c478e0e5b9ca4ba4c53
dotX12 <hash@shitposting.team>
2021-11-11 10:11:17 +0300

    added method get_data, finish_data. generate_key_state - current private _generate_key_data - current private _key_availability - current private

--- waio/storage/redis.py
+++ waio/storage/redis.py
@@ -12,11 +12,10 @@ class RedisStorage(RedisConnector):
         super().__init__(redis_url=redis_url, db_number=db)
         self.prefix_fsm = prefix_fsm
 
-    def generate_key_state(self, key) -> str:
-
+    def _generate_key_state(self, key) -> str:
         return f"{self.prefix_fsm}:{key}:state"
 
-    def generate_key_data(self, key) -> str:
+    def _generate_key_data(self, key) -> str:
         return f"{self.prefix_fsm}:{key}:data"
 
     async def _set(self, key: str, value: Any) -> bool:
@@ -24,32 +23,32 @@ class RedisStorage(RedisConnector):
         set_response = await self.redis.set(name=key, value=_value)
         return bool(set_response)
 
-    async def base_get(self, key) -> Dict[Union[str, int], Any]:
-        value = await self.redis.get(name=key)
-        value_dict = json.loads(value)
-        return value_dict
-
-    async def set(self, key, value) -> bool:
-        if await self.key_availability(key=key):
-            key_value = await self.base_get(key=key)
-            new_values = {**key_value, **value}
-            return await self._set(key, new_values)
-        else:
-            return await self._set(key, value)
-
-    async def key_availability(self, key) -> bool:
+    async def _key_availability(self, key) -> bool:
         key_exist = await self.redis.exists(key)
         if key_exist:
             return True
         return False
 
+    async def _base_get(self, key) -> Dict[Union[str, int], Any]:
+        value = await self.redis.get(name=key)
+        value_dict = json.loads(value)
+        return value_dict
+
+    async def set(self, key, value) -> bool:
+        if await self._key_availability(key=key):
+            key_value = await self._base_get(key=key)
+            new_values = {**key_value, **value}
+            return await self._set(key, new_values)
+        else:
+            return await self._set(key, value)
+
     async def set_state(self, key, value) -> bool:
-        fsm_key_state = self.generate_key_state(key=key)
+        fsm_key_state = self._generate_key_state(key=key)
         state_dict = {"state": str(value)}
         return await self.set(key=fsm_key_state, value=state_dict)
 
     async def get_state(self, key) -> Optional[str]:
-        key_state = self.generate_key_state(key=key)
+        key_state = self._generate_key_state(key=key)
         query = await self.redis.get(name=key_state)
         if query:
             query_state = json.loads(query)
@@ -58,10 +57,17 @@ class RedisStorage(RedisConnector):
         return None
 
     async def finish_state(self, key) -> None:
-        key_state = self.generate_key_state(key=key)
+        key_state = self._generate_key_state(key=key)
         await self.redis.delete(key_state)
 
     async def set_data(self, key, **kwargs) -> bool:
-        fsm_key_data = self.generate_key_data(key=key)
+        fsm_key_data = self._generate_key_data(key=key)
         return await self.set(key=fsm_key_data, value=kwargs)
 
+    async def get_data(self, key):
+        key = self._generate_key_data(key=key)
+        return await self._base_get(key)
+
+    async def finish_data(self, key) -> None:
+        key_data = self._generate_key_data(key=key)
+        await self.redis.delete(key_data)
