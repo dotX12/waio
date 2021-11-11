36051c45e1a0c0942aee0f1aff4dad83ea08c68a
dotX12 <hash@shitposting.team>
2021-11-11 23:34:26 +0300

    spelling error: you -> your

--- examples/fsm/main.py
+++ examples/fsm/main.py
@@ -24,7 +24,7 @@ class RegisterState(StateGroup):
 
 @dp.message_handler(commands=['register'], state='*')
 async def register_name(message: Message, state: FSMContext):
-    await message.answer(f'Hi, {message.sender_name}! Send you birthday date')
+    await message.answer(f'Hi, {message.sender_name}! send your date of birth')
     await state.set_state(RegisterState.birthday)
 
 
